87b01bdf2091c75a05aacb31bb9fdb4b0977e6c3
shriwise <pshriwise@gmail.com>
2017-07-31 10:46:19 -0500

    Adding tests for vbool. Particularly for movemask.

---
+++ tests/test_vbool.cpp
@@ -0,0 +1,85 @@
+
+#include "testutil.hpp"
+#include "vbool.h"
+
+void test_vbool4_constructors();
+void test_vbool4_mask();
+
+// void test_vbool4_operators();
+// void test_vbool4_comparators();
+// void test_vbool4_methods();
+
+int main( int argc, char** argv ) {
+
+  // test that values are correct after construction
+  test_vbool4_constructors();
+  // test vbool4 masking
+  test_vbool4_mask();
+  
+  return 0;
+}
+
+void test_vbool4_constructors() {
+
+  bool val = true;
+  vbool4 v(val);
+
+  CHECK_REAL_EQUAL(val, v[0], 0.0); 
+  CHECK_REAL_EQUAL(val, v[1], 0.0);
+  CHECK_REAL_EQUAL(val, v[2], 0.0);
+  CHECK_REAL_EQUAL(val, v[3], 0.0);
+
+  bool vals[4] = {true, false, true, false};
+  
+  v = vbool4(vals[0],vals[1], vals[2], vals[3]);
+  
+  CHECK_REAL_EQUAL(vals[0], v[0], 0.0); 
+  CHECK_REAL_EQUAL(vals[1], v[1], 0.0);
+  CHECK_REAL_EQUAL(vals[2], v[2], 0.0);
+  CHECK_REAL_EQUAL(vals[3], v[3], 0.0);
+  
+  const void* const p = (void*)vals;
+  v = vbool4::load(p);
+  
+  CHECK_REAL_EQUAL(vals[0], v[0], 0.0); 
+  CHECK_REAL_EQUAL(vals[1], v[1], 0.0);
+  CHECK_REAL_EQUAL(vals[2], v[2], 0.0);
+  CHECK_REAL_EQUAL(vals[3], v[3], 0.0);
+
+}
+
+void test_vbool4_mask() {
+  
+  vbool4 v = vbool4(true, true, true, true);
+  size_t result = movemask(v);
+  size_t expected_result = 15;
+  CHECK_EQUAL(expected_result,result);
+
+  v = vbool4(false, true, true, true);
+  result = movemask(v);
+  expected_result = 14;
+  CHECK_EQUAL(expected_result,result);
+
+  v = vbool4(false, false, true, true);
+  result = movemask(v);
+  expected_result = 12;
+  CHECK_EQUAL(expected_result,result);
+
+  v = vbool4(false, false, false, true);
+  result = movemask(v);
+  expected_result = 8;
+  CHECK_EQUAL(expected_result,result);
+
+  v = vbool4(false,false,false,false);
+  result = movemask(v);
+  expected_result = 0;
+  
+  v = vbool4(true,false,true,false);
+  result = movemask(v);
+  expected_result = 5;
+
+  v = vbool4(false,true,false,true);
+  result = movemask(v);
+  expected_result = 10;
+
+}
--- vbool.h
+++ vbool.h
@@ -22,9 +22,11 @@ struct vbool4
   inline vbool4( bool a )                        { b[0] = a; b[1] = a; b[2] = a; b[3] = a; }
   inline vbool4( bool i, bool j, bool k, bool l) { b[0] = i; b[1] = j; b[2] = k; b[3] = l; }
 
-  inline vbool4 load ( const void* const a) { for(size_t i=0; i < 4; i++) b[i] = *(bool*)((const char*)a+i*sizeof(bool)); }
-
-
+  static inline vbool4 load ( const void* const a) { vbool4 v;
+                                              for(size_t i=0; i < 4; i++) {
+						v.b[i] = *(bool*)((const char*)a+i*sizeof(bool));
+					      }
+                                              return v;}
   inline const bool& operator [](const size_t index) const { assert(index < 4); return b[index]; }
   inline       bool& operator [](const size_t index)       { assert(index < 4); return b[index]; }
 
@@ -40,48 +42,48 @@ inline const size_t movemask( const vbool4& v ) { return sgn(v.b[3]) << 3 | sgn(
 
 inline bool all( const vbool4& v ) { return v.b[0] && v.b[1] && v.b[2] && v.b[3]; }
 
-////////// Unary Ops //////////
-inline const vbool4 operator +( const vbool4& a ) { return a; }
-inline const vbool4 operator -( const vbool4& a ) { return vbool4(-a[0],-a[1],-a[2],-a[3]); }
+/* ////////// Unary Ops ////////// */
+/* inline const vbool4 operator +( const vbool4& a ) { return a; } */
+/* inline const vbool4 operator -( const vbool4& a ) { return vbool4(-a[0],-a[1],-a[2],-a[3]); } */
 
-////////// Binary Ops //////////
-inline const vbool4 operator +( const vbool4& a, const vbool4& b ) { return vbool4(a[0]+b[0],a[1]+b[1],a[2]+b[2],a[3]+b[3]); }
-inline const vbool4 operator +( const vbool4& a, const bool& b ) { return a+ vbool4(b); }
-inline const vbool4 operator +( const bool& a, const vbool4& b ) { return vbool4(a)+b; }
+/* ////////// Binary Ops ////////// */
+/* inline const vbool4 operator +( const vbool4& a, const vbool4& b ) { return vbool4(a[0]+b[0],a[1]+b[1],a[2]+b[2],a[3]+b[3]); } */
+/* inline const vbool4 operator +( const vbool4& a, const bool& b ) { return a+ vbool4(b); } */
+/* inline const vbool4 operator +( const bool& a, const vbool4& b ) { return vbool4(a)+b; } */
 
-inline const vbool4 operator -( const vbool4& a, const vbool4& b ) { return vbool4(a[0]-b[0],a[1]-b[1],a[2]-b[2],a[3]-b[3]); }
-inline const vbool4 operator -( const vbool4& a, const bool& b ) { return a-vbool4(b); }
-inline const vbool4 operator -( const bool& a, const vbool4& b ) { return vbool4(a)-b; }
+/* inline const vbool4 operator -( const vbool4& a, const vbool4& b ) { return vbool4(a[0]-b[0],a[1]-b[1],a[2]-b[2],a[3]-b[3]); } */
+/* inline const vbool4 operator -( const vbool4& a, const bool& b ) { return a-vbool4(b); } */
+/* inline const vbool4 operator -( const bool& a, const vbool4& b ) { return vbool4(a)-b; } */
 
-inline const vbool4 operator *( const vbool4& a, const vbool4& b ) { return vbool4(a[0]*b[0],a[1]*b[1],a[2]*b[2],a[3]*b[3]); }
-inline const vbool4 operator *( const vbool4& a, const bool& b ) { return a*vbool4(b); }
-inline const vbool4 operator *( const bool& a, const vbool4& b ) { return vbool4(a)*b; }
+/* inline const vbool4 operator *( const vbool4& a, const vbool4& b ) { return vbool4(a[0]*b[0],a[1]*b[1],a[2]*b[2],a[3]*b[3]); } */
+/* inline const vbool4 operator *( const vbool4& a, const bool& b ) { return a*vbool4(b); } */
+/* inline const vbool4 operator *( const bool& a, const vbool4& b ) { return vbool4(a)*b; } */
 
-inline const vbool4 operator /( const vbool4& a, const vbool4& b ) { return vbool4(a[0]/b[0],a[1]/b[1],a[2]/b[2],a[3]/b[3]); }
-inline const vbool4 operator /( const vbool4& a, const bool& b ) { return a/vbool4(b); }
-inline const vbool4 operator /( const bool& a, const vbool4& b ) { return vbool4(a)/b; }
+/* inline const vbool4 operator /( const vbool4& a, const vbool4& b ) { return vbool4(a[0]/b[0],a[1]/b[1],a[2]/b[2],a[3]/b[3]); } */
+/* inline const vbool4 operator /( const vbool4& a, const bool& b ) { return a/vbool4(b); } */
+/* inline const vbool4 operator /( const bool& a, const vbool4& b ) { return vbool4(a)/b; } */
 
-////////// Assignment Ops //////////
-inline vbool4& operator +=( vbool4& a, const vbool4& b ) { return a = a + b; }
-inline vbool4& operator +=( vbool4& a, const bool&   b ) { return a = a + b; }
+/* ////////// Assignment Ops ////////// */
+/* inline vbool4& operator +=( vbool4& a, const vbool4& b ) { return a = a + b; } */
+/* inline vbool4& operator +=( vbool4& a, const bool&   b ) { return a = a + b; } */
 
-inline vbool4& operator -=( vbool4& a, const vbool4& b ) { return a = a - b; }
-inline vbool4& operator -=( vbool4& a, const bool&   b ) { return a = a - b; }
+/* inline vbool4& operator -=( vbool4& a, const vbool4& b ) { return a = a - b; } */
+/* inline vbool4& operator -=( vbool4& a, const bool&   b ) { return a = a - b; } */
 
-inline vbool4& operator *=( vbool4& a, const vbool4& b ) { return a = a * b; }
-inline vbool4& operator *=( vbool4& a, const bool&   b ) { return a = a * b; }
+/* inline vbool4& operator *=( vbool4& a, const vbool4& b ) { return a = a * b; } */
+/* inline vbool4& operator *=( vbool4& a, const bool&   b ) { return a = a * b; } */
 
-inline vbool4& operator /=( vbool4& a, const vbool4& b ) { return a = a / b; }
-inline vbool4& operator /=( vbool4& a, const bool&   b ) { return a = a / b; }                       
+/* inline vbool4& operator /=( vbool4& a, const vbool4& b ) { return a = a / b; } */
+/* inline vbool4& operator /=( vbool4& a, const bool&   b ) { return a = a / b; }                        */
 
-////////// Other Common Ops //////////
-inline const vbool4 madd  ( const vbool4& a, const vbool4& b, const vbool4& c) { return a*b+c; }
-inline const vbool4 msub  ( const vbool4& a, const vbool4& b, const vbool4& c) { return a*b-c; }
-inline const vbool4 nmadd ( const vbool4& a, const vbool4& b, const vbool4& c) { return -a*b+c;}
-inline const vbool4 nmsub ( const vbool4& a, const vbool4& b, const vbool4& c) { return -a*b-c; }       
+/* ////////// Other Common Ops ////////// */
+/* inline const vbool4 madd  ( const vbool4& a, const vbool4& b, const vbool4& c) { return a*b+c; } */
+/* inline const vbool4 msub  ( const vbool4& a, const vbool4& b, const vbool4& c) { return a*b-c; } */
+/* inline const vbool4 nmadd ( const vbool4& a, const vbool4& b, const vbool4& c) { return -a*b+c;} */
+/* inline const vbool4 nmsub ( const vbool4& a, const vbool4& b, const vbool4& c) { return -a*b-c; }        */
 
-inline bool min(const vbool4& v ) { return std::min(std::min(v.b[0],v.b[1]),std::min(v.b[2],v.b[3])); }
-inline bool max(const vbool4& v ) { return std::max(std::max(v.b[0],v.b[1]),std::max(v.b[2],v.b[3])); }
+/* inline bool min(const vbool4& v ) { return std::min(std::min(v.b[0],v.b[1]),std::min(v.b[2],v.b[3])); } */
+/* inline bool max(const vbool4& v ) { return std::max(std::max(v.b[0],v.b[1]),std::max(v.b[2],v.b[3])); } */
 
 
 inline std::ostream& operator<<(std::ostream& cout, const vbool4& a) {
