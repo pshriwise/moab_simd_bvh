39da4d3de631fa6270a8af696818d905d1bc5d82
shriwise <pshriwise@gmail.com>
2017-08-24 11:13:19 -0500

    Filling in the build fuction for thew BVHBuilder class.

--- src/Builder.h
+++ src/Builder.h
@@ -11,7 +11,9 @@ enum BuildQuality {
   
 struct BuildPrimitive {
   float lower_x, lower_y, lower_z;
+  int sceneID;
   float upper_x, upper_y, upper_z;
+  int geomID;
 
   Vec3fa center() const { return (Vec3fa(lower_x,lower_y,lower_z)+Vec3fa(upper_x,upper_y,upper_z))/2.0f; }
 };
@@ -91,10 +93,15 @@ void sortPrimitives(const int dim, const float coord,
 
 
 
+struct TempNode {
+  AABB box;
+  std::vector<BuildPrimitive> prims;
+
+  float sah_contribution() { return area(box)*(float)prims.size(); }
+};
 
 
-
-void splitNode(NodeRef* node, size_t split_axis, const BuildPrimitive* primitives, const size_t numPrimitives) {
+void splitNode(NodeRef* node, size_t split_axis, const BuildPrimitive* primitives, const size_t numPrimitives, TempNode tn[N]) {
 
   assert(split_axis >= 0 && split_axis <= 2);
 
@@ -121,33 +128,86 @@ void splitNode(NodeRef* node, size_t split_axis, const BuildPrimitive* primitive
   bounds[2*split_axis] = vfloat4(lb, lb + delta, lb + 2*delta, lb + 3*delta);
   bounds[2*split_axis+1] = vfloat4(lb + delta, lb + 2*delta, lb + 3*delta, lb + 4*delta);
 
-  AABB boxes[4];
-  boxes[0] = AABB(bounds[0][0], bounds[2][0], bounds[4][0], bounds[1][0], bounds[3][0], bounds[5][0]);
-  boxes[1] = AABB(bounds[0][1], bounds[2][1], bounds[4][1], bounds[1][1], bounds[3][1], bounds[5][1]);
-  boxes[2] = AABB(bounds[0][2], bounds[2][2], bounds[4][2], bounds[1][2], bounds[3][2], bounds[5][2]);
-  boxes[3] = AABB(bounds[0][3], bounds[2][3], bounds[4][3], bounds[1][3], bounds[3][3], bounds[5][3]);
+  AABB boxes[N];
 
+  boxes[0] = AABB(bounds[0][0],
+		   bounds[2][0],
+		   bounds[4][0],
+		   bounds[1][0],
+		   bounds[3][0],
+		   bounds[5][0]);
+  boxes[1] = AABB(bounds[0][1],
+		   bounds[2][1],
+		   bounds[4][1],
+		   bounds[1][1],
+		   bounds[3][1],
+		   bounds[5][1]);
+  boxes[2] = AABB(bounds[0][2],
+		   bounds[2][2],
+		   bounds[4][2],
+		   bounds[1][2],
+		   bounds[3][2],
+		   bounds[5][2]);
+  boxes[3] = AABB(bounds[0][3],
+		   bounds[2][3],
+		   bounds[4][3],
+		   bounds[1][3],
+		   bounds[3][3],
+		   bounds[5][3]);
 
+  
   // sort primitives into boxes by their centroid
   for(size_t i = 0; i < numPrimitives; i++) {
     BuildPrimitive p = primitives[i];
     bool placed = false;
     for(size_t j = 0; j < 4 ; j++) {
       // if the centroid is in the box, place it there
-      if( inside(boxes[j], p.center()) ){
+      if( inside(tn[j].box, p.center()) ){
 	placed = true;
+	tn[j].prims.push_back(p);
+	tn[j].box.update(p.lower_x, p.lower_y, p.lower_z);
+	tn[j].box.update(p.upper_x, p.upper_y, p.upper_z);
       }
     }
     assert(placed);
   }
+
 }
 
-struct TempNode {
-  AABB box;
-  std::vector<BuildPrimitive> prims;
 
-  float sah_contribution() { return area(box)*(float)prims.size(); }
-};
+void splitNode(NodeRef* node, const BuildPrimitive* primitives, const size_t numPrimitives) {
+
+  // split node along each axis
+  float max_cost = 0.0;
+  float min_cost = 1.0;
+  
+  float best_cost = max_cost;
+  float cost;
+
+  AANode* this_node = node->node();
+
+  AABB node_box = this_node->bounds();
+  
+  TempNode tempNodes[4];
+  
+  // split along each axis and get lowest cost
+  for(size_t i = 0; i < 3; i++) {
+    splitNode(node, i, primitives, numPrimitives, tempNodes);
+    //compute the SAH cost of this node split
+    cost = tempNodes[0].sah_contribution() +
+           tempNodes[1].sah_contribution() +
+           tempNodes[2].sah_contribution() +
+           tempNodes[3].sah_contribution();
+    cost /= area(node_box)*(float)numPrimitives;
+
+    assert(cost >= min_cost && cost <= max_cost);
+    // update cost
+    if (cost < best_cost) {
+      best_cost = cost;
+    }
+  }
+  
+}
 
 struct PrimRef{
   inline PrimRef () {}
@@ -211,12 +271,15 @@ struct BuildState
 
 class BVHBuilder {
 
+ BVHBuilder() : maxLeafSize(8) {}
+  
  private:
   std::vector<BuildPrimitive> primitive_vec;
-
+  size_t maxLeafSize;
+  
  public:
   
-  NodeRef Build(const BuildSettings& settings,
+  NodeRef* Build(const BuildSettings& settings,
 		 BuildPrimitive* primitives,
 		 size_t numPrimitives,
 		 //	    createNodeFunc createNode,
@@ -230,13 +293,19 @@ class BVHBuilder {
     vptr = primitives;
 
     
-    
-    
-    
-    
-
-    NodeRef* node;
-    createNode(node);
-    
+    if(numPrimitives <= maxLeafSize) {
+      return (NodeRef*)createLeaf(primitives, numPrimitives);
+    }
+    else{
+      AANode* aanode = new AANode();
+      AABB box;
+      for(size_t i = 0; i < numPrimitives; i++) {
+	box.extend(*(AABB*)(void*)&(primitives[i]));
+      }
+      aanode->setBounds(box);
+      NodeRef* this_node = new NodeRef((size_t)aanode);
+      splitNode(this_node, primitives, numPrimitives);
+    }
+        
   } // end builder
 };
