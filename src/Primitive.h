8614b2ac1b5b26048bed13849132817985898348
shriwise <pshriwise@gmail.com>
2017-09-13 14:50:06 -0500

    Adding comparator for BuildPrimitive.

--- src/Primitive.h
+++ src/Primitive.h
@@ -11,6 +11,8 @@ struct BuildPrimitive {
   int primID;
 
   friend bool operator< (const BuildPrimitive& a, const BuildPrimitive& b) { return a.primID < b.primID; }
-  
+
+  friend bool operator!= (const BuildPrimitive& a, const BuildPrimitive& b) { return a.primID != b.primID; }
+
   Vec3fa center() const { return (Vec3fa(lower_x,lower_y,lower_z)+Vec3fa(upper_x,upper_y,upper_z))/2.0f; }
 };
