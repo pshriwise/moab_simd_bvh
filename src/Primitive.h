d9a507ef78f4e4ccdb6483847e28544e4065ac1d
shriwise <pshriwise@gmail.com>
2017-09-18 15:54:42 -0500

    Adding string representation for BuildPrimitive.

--- src/Primitive.h
+++ src/Primitive.h
@@ -23,3 +23,15 @@ struct BuildPrimitive {
   
   Vec3fa center() const { return (Vec3fa(lower_x,lower_y,lower_z)+Vec3fa(upper_x,upper_y,upper_z))/2.0f; }
 };
+
+
+inline std::ostream& operator<< (std::ostream &os, BuildPrimitive &p) {
+  return os <<  "Primitive "  << p.primID << std::endl <<
+                "Lower xyz: " << p.lower_x << " "
+	                      << p.lower_y << " "
+                              << p.lower_z << std::endl <<
+                "Upper xyz: " << p.upper_x << " "
+                              << p.upper_y << " "
+                              << p.upper_z << std::endl <<
+                "Part of Scene " << p.sceneID << std::endl;
+}
