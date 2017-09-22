3afd1219f252de7745856de367239284034fb7e4
shriwise <pshriwise@gmail.com>
2017-09-22 08:44:16 -0500

    Adding a test program for the BuildRecord struct

---
+++ tests/test_buildrecord.cpp
@@ -0,0 +1,21 @@
+
+#include <stdio.h>
+
+#include "testutil.hpp"
+#include "BuildState.h"
+
+void test_buildrecord();
+
+int main(int argc, char** argv) {
+
+  test_buildrecord();
+
+  return 0;
+}
+
+void test_buildrecord() {
+
+  
+
+  return;
+}
