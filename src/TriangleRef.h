cb2029d09d85bbb9d9d5dd94bfe7100908a86f1d
shriwise <pshriwise@gmail.com>
2017-10-03 23:32:14 -0500

    Adding a test for a specific distance for the cube geometry.

--- src/TriangleRef.h
+++ src/TriangleRef.h
@@ -34,14 +34,25 @@ struct TriangleRef : public BuildPrimitive {
     rval = mbi->get_coords(&(conn[0]), 3, coords[0].array() );
     MB_CHK_SET_ERR_RET(rval, "Failed to get triangle vert coords");
 
-    
+
+    const float round_down = 1.0f-2.0f*float(ulp); // FIXME: use per instruction rounding for AVX512
+    const float round_up   = 1.0f+2.0f*float(ulp);
+
     upper.x = std::max(coords[0][0],std::max(coords[1][0], coords[2][0]));
+    upper.x *= round_up;
     upper.y = std::max(coords[0][1],std::max(coords[1][1], coords[2][1]));
+    upper.y *= round_up;
     upper.z = std::max(coords[0][2],std::max(coords[1][2], coords[2][2]));
-		     
+    upper.z *= round_up;
+    
     lower.x = std::min(coords[0][0],std::min(coords[1][0], coords[2][0]));
+    lower.x *= round_down;
     lower.y = std::min(coords[0][1],std::min(coords[1][1], coords[2][1]));
+    lower.y *= round_down;
     lower.z = std::min(coords[0][2],std::min(coords[1][2], coords[2][2]));
+    lower.z *= round_down;
+    	
+
     
   }
 
