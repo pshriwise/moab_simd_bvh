a32ff3bbf1b74f61b97ff3e3169e792c94e5fb1d
shriwise <pshriwise@gmail.com>
2017-10-24 14:11:01 -0500

    Adding start of a new test for system robustness.

---
+++ tests/test_moab_robust.cpp
@@ -0,0 +1,20 @@
+
+#define TEST_SPHERE "${CMAKE_CURRENT_BINARY_DIR}/test_files/sphere.h5m"
+
+#include "assert.h"
+
+#include "moab/Core.hpp"
+#include "moab/CartVect.hpp"
+#include "moab/Range.hpp"
+
+#include "MBVH.h"
+
+#include "testutil.hpp"
+
+int main(int argc, char** argv) {
+
+  moab::ErrorCode rval = moab::MB_FAILURE;  
+
+  return rval;
+}
+
