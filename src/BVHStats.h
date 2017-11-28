45a4c11284d2bee08bdf539967e8b0e601f9a06e
shriwise <pshriwise@gmail.com>
2017-11-28 15:42:22 -0600

    Adding more stats to output of the tracker

--- src/BVHStats.h
+++ src/BVHStats.h
@@ -11,13 +11,30 @@ struct BVHStatTracker {
   
   std::vector<int> s;
 
+  
   int num_empty;
   int num_leaves;
   int depth;
+  int max_leaf;
+  int min_leaf;
+  int total_entities;
+
+  int min_leaf_depth;
+  int max_leaf_depth;
+  int leaf_depth_agg;
   
   static const size_t stackSize = 1+(N-1)*BVH_MAX_DEPTH;
 
-  inline BVHStatTracker() : num_empty(0), num_leaves(0), depth(0) { s.push_back(1); }
+  inline BVHStatTracker() : num_empty(0),
+			    num_leaves(0),
+			    depth(0),
+			    min_leaf(inf),
+			    max_leaf(0),
+			    total_entities(0),
+			    min_leaf_depth(inf),
+			    max_leaf_depth(0),
+			    leaf_depth_agg(0)
+  { s.push_back(1); }
 
   inline void count_hits(size_t mask, int& hits) {
     assert(mask <= 15 && mask >= 0);
@@ -99,12 +116,26 @@ struct BVHStatTracker {
 
 	size_t numPrims;
 	void* prims = cur.leaf(numPrims);
+	max_leaf = numPrims > max_leaf ? numPrims : max_leaf;
+	min_leaf = numPrims > 0 && numPrims < min_leaf ? numPrims : min_leaf;
 
+	max_leaf_depth = current_depth() > max_leaf_depth ? current_depth() : max_leaf_depth;
+	min_leaf_depth = current_depth() < min_leaf_depth ? current_depth() : min_leaf_depth;
+	
+	total_entities += numPrims;
+	leaf_depth_agg += current_depth();
       }
 
+    std::cout << "Tree depth: " << depth << std::endl;
+    std::cout << "Total number of entiites in the tree: " << total_entities << std::endl;
     std::cout << "Number of leaves in the tree: " << num_leaves << std::endl;
     std::cout << "Number of empty nodes in the tree: " << num_empty << std::endl;
-    std::cout << "Tree depth: " << depth << std::endl;
+    std::cout << "Minimum entities in non-empty leaf: " << min_leaf << std::endl;
+    std::cout << "Maximum entities in leaf: " << max_leaf << std::endl;
+    std::cout << "Average entities in leaf: " << (double)total_entities/(double)num_leaves << std::endl;
+    std::cout << "Maximum leaf depth: " << max_leaf_depth << std::endl;
+    std::cout << "Minimum leaf depth: " << min_leaf_depth << std::endl;
+    std::cout << "Average leaf depth: " << (double)leaf_depth_agg/(double)num_leaves << std::endl;
   }
   
 };
