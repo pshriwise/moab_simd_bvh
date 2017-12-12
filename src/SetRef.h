28eb7b98cc0ecebcb83f9dee74f8c930ede0aa62
shriwise <pshriwise@gmail.com>
2017-12-12 11:31:58 -0600

    Adding SetRef struct

---
+++ src/SetRef.h
@@ -0,0 +1,15 @@
+
+
+template<typename I>
+struct SetRefT {
+
+  SetRefT(NodeRef* node, I set_id, I fwd_id, I rev_id) : n(node), setID(set_id), fwdID(fwd_id), revID(rev_id) {}
+  // pointer to the tree
+  NodeRef *n;
+
+  // set handle and sense information
+  I setID, fwdID, revID;
+  
+};
+
+typedef SetRefT<unsigned> SetRef;
