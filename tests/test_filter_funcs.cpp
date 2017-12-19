1f9e1df495926055d25b6cf51db6d954310f8bb8
shriwise <pshriwise@gmail.com>
2017-12-19 09:25:58 -0600

    Start of a test for filter functions

---
+++ tests/test_filter_funcs.cpp
@@ -0,0 +1,19 @@
+
+
+#include "testutil.hpp"
+
+
+#include "MBVHManager.h"
+#include "moab/Core.hpp"
+
+
+
+
+int main(int argc, char** argv) {
+
+
+  moab::ErrorCode rval = moab::MB_SUCCESS;
+  
+  return rval;
+
+}
