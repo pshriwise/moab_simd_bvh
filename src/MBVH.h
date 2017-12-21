eacee19232b0f12af73f3f35b2ce32d15d4fc982
shriwise <pshriwise@gmail.com>
2017-12-21 15:53:37 -0600

    Reducing this file to two lines required to define the MOAB BVH.

--- src/MBVH.h
+++ src/MBVH.h
@@ -2,13 +2,5 @@
 
 #include "BVH.h"
 
-/* typedef BVHBuilder<TriangleRef> TriangleBVH; */
-
-/* typedef BuildStateT<TriangleRef> BuildStateTri; */
-
-/* typedef BVHIntersectorT<TriangleRef, Vec3fa, float, int> TriIntersector; */
-
-/* typedef BVHIntersectorT<TriangleRef, Vec3da, double, int> DblTriIntersector; */
-
 typedef RayT<Vec3da, double, moab::EntityHandle> MBRay;
 typedef BVH<Vec3da, double, moab::EntityHandle, MBTriangleRef> MBVH;
