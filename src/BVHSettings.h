f20264e8d9b7df5e046da4c2b2e67c4f82d01784
shriwise <pshriwise@gmail.com>
2017-12-21 11:16:09 -0600

    Cleaning the BVHSettings struct and updating its tests.

--- src/BVHSettings.h
+++ src/BVHSettings.h
@@ -13,13 +13,15 @@ enum BVH_HEURISTIC { ENTITY_RATIO_HEURISTIC = 0,
 template<typename T>
 struct BVHSettings {
 
+  float (*evaluate_cost)(TempNode<T> tempNodes[N], const AABB &node_box, const size_t &numPrimitives);
+  
+  // constructor
   BVHSettings() {
+    // set SAH by default
     evaluate_cost = &surface_area_heuristic;
   }
-  		       
-  float (*evaluate_cost)(TempNode<T> tempNodes[N], const AABB &node_box, const size_t &numPrimitives);
 
-
+  // implementation of the entity ratio heuristic (QUAD TREE ONLY RN)
   static float entity_ratio_heuristic(TempNode<T> tempNodes[N], const AABB &node_box, const size_t &numPrimitives) {
     float cost = abs(abs(tempNodes[0].size() - tempNodes[1].size()) - abs(tempNodes[2].size() - tempNodes[3].size()));
     int total = 0;
@@ -27,6 +29,7 @@ struct BVHSettings {
     return cost /= (float)total;
   }
 
+  // implementation of the surface area heuristic
   static float surface_area_heuristic(TempNode<T> tempNodes[N], const AABB &node_box, const size_t &numPrimitives) {
     float cost = 0;
     for(size_t i = 0; i < N; i++) {
@@ -35,25 +38,26 @@ struct BVHSettings {
     return cost /= ( (float)numPrimitives * area(node_box) );
   }
 
+  // method for switching between heuristics
   void set_heuristic(BVH_HEURISTIC h) {
-
     switch(h) {
+    // set heuristic evaluation pointer to entity ratio
     case ENTITY_RATIO_HEURISTIC:
       {
 	evaluate_cost = &entity_ratio_heuristic;
 	break;
       }
+    // set heuristic evaluation pointer to surface area heuristic
     case SURFACE_AREA_HEURISTIC:
       {
 	evaluate_cost = &surface_area_heuristic;
 	break;
       }
+    // if an invalid heuristic type is specified, report to user and do nothing
     default:
       std::cout << "INVALID HEURISTIC" << std::endl;
     }
-    
   }
-
 };
 
 
--- tests/test_buildsettings.cpp
+++ tests/test_buildsettings.cpp
@@ -9,28 +9,29 @@ int main(int argc, char** argv) {
 
   BVHSettings<int> settings;
 
-  settings.set_heuristic(ENTITY_RATIO_HEURISTIC);
-
   TempNode<int> tempNodes[4];
 
   float cost;
   float expected_cost;
-  // setup the nodes for a test
+  AABB dummy_box;
+  size_t numPrims = 100;
+
+  /* Entity Ratio Heuristic Tests */
+  settings.set_heuristic(ENTITY_RATIO_HEURISTIC);
+  
+  // test with an even split
   tempNodes[0].prims.resize(25);
   tempNodes[1].prims.resize(25);
   tempNodes[2].prims.resize(25);
   tempNodes[3].prims.resize(25);
 
-  AABB dummy_box;
-  size_t numPrims = 100;
-  
   expected_cost = 0.0;
   cost = settings.entity_ratio_heuristic(tempNodes, dummy_box, numPrims);
   CHECK_REAL_EQUAL(expected_cost, cost, 0.0f);
   cost = settings.evaluate_cost(tempNodes, dummy_box, numPrims);
   CHECK_REAL_EQUAL(expected_cost, cost, 0.0f);
 
-  // next test
+  // test for a slightly skewed split
   tempNodes[0].prims.resize(30);
   tempNodes[1].prims.resize(20);
   tempNodes[2].prims.resize(25);
@@ -40,33 +41,34 @@ int main(int argc, char** argv) {
   expected_cost = 0.1f;
   CHECK_REAL_EQUAL(expected_cost, cost, 0.0f);
 
-  //change to Surface Area Heuristic
+  /* Surface Area Heuristic Tests */
   settings.set_heuristic(SURFACE_AREA_HEURISTIC);
-
   
-  // setup the nodes for a test
+  // test an even split
   tempNodes[0].prims.resize(25);
   tempNodes[1].prims.resize(25);
   tempNodes[2].prims.resize(25);
   tempNodes[3].prims.resize(25);
 
+  // set the box sizes
   new (&tempNodes[0].box) AABB(-20.0f, -10.0f);
   new (&tempNodes[1].box) AABB(-10.0f,   0.0f);
   new (&tempNodes[2].box) AABB(  0.0f,  10.0f);
   new (&tempNodes[3].box) AABB( 10.0f,  20.0f);
 
+  // mimic a parent box
   AABB node_box;
   node_box.extend(tempNodes[0].box);
   node_box.extend(tempNodes[1].box);
   node_box.extend(tempNodes[2].box);
   node_box.extend(tempNodes[3].box);
-
+  
+  // test
   expected_cost = 0.0625f;
   cost = settings.evaluate_cost(tempNodes, node_box, numPrims);
- 
   CHECK_REAL_EQUAL(expected_cost, cost, 0.0f);
 
-  // vary entities
+  // test a slightly skewed split in number of entities
   tempNodes[0].prims.resize(30);
   tempNodes[1].prims.resize(20);
   tempNodes[2].prims.resize(25);
@@ -77,9 +79,33 @@ int main(int argc, char** argv) {
   new (&tempNodes[2].box) AABB(  0.0f,  10.0f);
   new (&tempNodes[3].box) AABB( 10.0f,  20.0f);
 
+  //test
   expected_cost = 0.0625f;
   cost = settings.evaluate_cost(tempNodes, node_box, numPrims);  
   CHECK_REAL_EQUAL(expected_cost, cost, 0.0f);
+
+  // test a slightly skewed split in number of entities
+  tempNodes[0].prims.resize(30);
+  tempNodes[1].prims.resize(20);
+  tempNodes[2].prims.resize(25);
+  tempNodes[3].prims.resize(25);
+
+  // with different sized boxes
+  new (&tempNodes[0].box) AABB(-15.0f, -10.0f);
+  new (&tempNodes[1].box) AABB(-10.0f,   0.0f);
+  new (&tempNodes[2].box) AABB(  0.0f,  10.0f);
+  new (&tempNodes[3].box) AABB( 10.0f,  15.0f);
+
+  node_box.clear();
+  node_box.extend(tempNodes[0].box);
+  node_box.extend(tempNodes[1].box);
+  node_box.extend(tempNodes[2].box);
+  node_box.extend(tempNodes[3].box);
   
+  //test
+  expected_cost = 0.065277778f;
+  cost = settings.evaluate_cost(tempNodes, node_box, numPrims);
+  CHECK_REAL_EQUAL(expected_cost, cost, 0.0f);
+
   return 0;
 }
