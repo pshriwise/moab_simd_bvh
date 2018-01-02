0a2c92fa9976c4b2beedee19fef7648e9be1083b
shriwise <pshriwise@gmail.com>
2018-01-02 12:00:06 -0600

    Applying some explicit SIMD instructions to the vfloat 4 struct

--- src/vfloat.h
+++ src/vfloat.h
@@ -5,13 +5,20 @@
 #include "Vec3.h"
 #include "math.h"
 #include "vbool.h"
+#include <xmmintrin.h>
 
 struct vfloat4
 {
 
   enum { size = 4 }; // number of elements
-  float f[4]; // data holder
+  union { __m128 v; float f[4]; int i[4]; }; // data holder
 
+  //  inline vfloat4 (float a) : v(_mm_set1_ps(a) {}
+  inline vfloat4 (const __m128 a) : v(a) {}
+
+  inline operator const __m128&( void ) const { return v; }
+  inline operator       __m128&( void )       { return v; }
+  
   inline vfloat4 () {}
   inline vfloat4 (const vfloat4& other) { f[0] = other.f[0];
                                           f[1] = other.f[1];
@@ -26,18 +33,10 @@ struct vfloat4
   inline const float& operator [](const size_t index) const { assert(index < 4); return f[index]; }
   inline       float& operator [](const size_t index)       { assert(index < 4); return f[index]; }
 
-  static inline vfloat4 load ( const void* const a) { vfloat4 v;
-                                               for(size_t i=0; i < 4; i++) {
-						 v.f[i] = *(float*)((const char*)a+i*sizeof(float));
-					       }
-					       return v; }
+  static inline vfloat4 load ( const void* const a) { return _mm_load_ps((float*)a); }
 
-  static inline void store ( void* ptr, const vfloat4&v ) {
-                                               vfloat4* p = (vfloat4*) ptr;
-                                               for(size_t i=0; i < 4; i++) {
-						 (*p).f[i] = v[i];
-					       }
-                            } 
+
+  static inline void store ( void* ptr, const vfloat4&v ) { return _mm_store_ps((float*)ptr, v); }
 
 };
 
@@ -47,19 +46,19 @@ inline const vfloat4 operator +( const vfloat4& a ) { return a; }
 inline const vfloat4 operator -( const vfloat4& a ) { return vfloat4(-a[0],-a[1],-a[2],-a[3]); }
 
 ////////// Binary Ops //////////
-inline const vfloat4 operator +( const vfloat4& a, const vfloat4& b ) { return vfloat4(a[0]+b[0],a[1]+b[1],a[2]+b[2],a[3]+b[3]); }
-inline const vfloat4 operator +( const vfloat4& a, const float& b ) { return a+ vfloat4(b); }
-inline const vfloat4 operator +( const float& a, const vfloat4& b ) { return vfloat4(a)+b; }
+inline const vfloat4 operator +( const vfloat4& a, const vfloat4& b ) { return _mm_add_ps(a.v, b.v); }
+inline const vfloat4 operator +( const vfloat4& a, const float& b ) { return a + vfloat4(b); }
+inline const vfloat4 operator +( const float& a, const vfloat4& b ) { return vfloat4(a) + b; }
 
-inline const vfloat4 operator -( const vfloat4& a, const vfloat4& b ) { return vfloat4(a[0]-b[0],a[1]-b[1],a[2]-b[2],a[3]-b[3]); }
-inline const vfloat4 operator -( const vfloat4& a, const float& b ) { return a-vfloat4(b); }
+inline const vfloat4 operator -( const vfloat4& a, const vfloat4& b ) { return _mm_sub_ps(a.v, b.v); }
+inline const vfloat4 operator -( const vfloat4& a, const float& b ) { return a - vfloat4(b); }
 inline const vfloat4 operator -( const float& a, const vfloat4& b ) { return vfloat4(a)-b; }
 
-inline const vfloat4 operator *( const vfloat4& a, const vfloat4& b ) { return vfloat4(a[0]*b[0],a[1]*b[1],a[2]*b[2],a[3]*b[3]); }
+inline const vfloat4 operator *( const vfloat4& a, const vfloat4& b ) { return _mm_mul_ps(a.v, b.v); }
 inline const vfloat4 operator *( const vfloat4& a, const float& b ) { return a*vfloat4(b); }
 inline const vfloat4 operator *( const float& a, const vfloat4& b ) { return vfloat4(a)*b; }
 
-inline const vfloat4 operator /( const vfloat4& a, const vfloat4& b ) { return vfloat4(a[0]/b[0],a[1]/b[1],a[2]/b[2],a[3]/b[3]); }
+inline const vfloat4 operator /( const vfloat4& a, const vfloat4& b ) { return _mm_div_ps(a.v, b.v); }
 inline const vfloat4 operator /( const vfloat4& a, const float& b ) { return a/vfloat4(b); }
 inline const vfloat4 operator /( const float& a, const vfloat4& b ) { return vfloat4(a)/b; }
 
@@ -112,23 +111,25 @@ inline const vfloat4 nmsub ( const vfloat4& a, const vfloat4& b, const vfloat4& 
 inline float min(const vfloat4& v ) { return std::min(std::min(v.f[0],v.f[1]),std::min(v.f[2],v.f[3])); }
 inline float max(const vfloat4& v ) { return std::max(std::max(v.f[0],v.f[1]),std::max(v.f[2],v.f[3])); }
 
-inline vfloat4 min(const vfloat4& a, const vfloat4&b) { return vfloat4(std::min(a.f[0],b.f[0]),
-				                                       std::min(a.f[1],b.f[1]),
-								       std::min(a.f[2],b.f[2]),
-								       std::min(a.f[3],b.f[3])); }
+inline vfloat4 min(const vfloat4& a, const vfloat4&b) { return _mm_min_ps(a.v, b.v); }
+  /* return vfloat4(std::min(a.f[0],b.f[0]), */
+  /* 		 std::min(a.f[1],b.f[1]), */
+  /* 		 std::min(a.f[2],b.f[2]), */
+  /* 		 std::min(a.f[3],b.f[3])); } */
 
 inline vfloat4 min(const vfloat4& a, const vfloat4&b, const vfloat4& c) { return min(min(a,b),c); }
 
 inline vfloat4 min(const vfloat4& a, const vfloat4&b, const vfloat4& c, const vfloat4& d) { return min(min(a,b),c,d); }
 
 
-inline vfloat4 max(const vfloat4& a, const vfloat4&b) { vfloat4 v;
-                                                        v.f[0] = std::max(a.f[0],b.f[0]);
-                                                        v.f[1] = std::max(a.f[1],b.f[1]);
-                                                        v.f[2] = std::max(a.f[2],b.f[2]);
-                                                        v.f[3] = std::max(a.f[3],b.f[3]);
-							return v;
-                                                       }
+inline vfloat4 max(const vfloat4& a, const vfloat4&b) { return _mm_max_ps(a.v, b.v); }
+/*   vfloat4 v; */
+/*   v.f[0] = std::max(a.f[0],b.f[0]); */
+/*   v.f[1] = std::max(a.f[1],b.f[1]); */
+/*   v.f[2] = std::max(a.f[2],b.f[2]); */
+/*   v.f[3] = std::max(a.f[3],b.f[3]); */
+/*   return v; */
+/* } */
 
 inline vfloat4 max(const vfloat4& a, const vfloat4&b, const vfloat4& c) { return max(max(a,b),c); }
 
