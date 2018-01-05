0c8ec0b82dafd8aae9af02e6d486d2823ebd6437
shriwise <pshriwise@gmail.com>
2018-01-05 17:38:22 -0600

    Forcing the inlining of other utility functions

--- src/Vec3.h
+++ src/Vec3.h
@@ -12,46 +12,46 @@ template<typename T> struct Vec3 {
 
   T x,y,z;
   
-  inline Vec3() {}
+  __forceinline Vec3() {}
     
-  inline Vec3( const Vec3 &other) { x = other.x;
+  __forceinline Vec3( const Vec3 &other) { x = other.x;
                                     y = other.y;
 				    z = other.z; }
 
-  inline Vec3(const Vec3fa&other) : x(T(other.x)), y(T(other.y)), z(T(other.z)) {}
-  inline Vec3(const Vec3da&other) : x(T(other.x)), y(T(other.y)), z(T(other.z)) {}
+  __forceinline Vec3(const Vec3fa&other) : x(T(other.x)), y(T(other.y)), z(T(other.z)) {}
+  __forceinline Vec3(const Vec3da&other) : x(T(other.x)), y(T(other.y)), z(T(other.z)) {}
   
-  /* inline Vec3( const Vec3fa& other) { x = other.x; */
+  /* __forceinline Vec3( const Vec3fa& other) { x = other.x; */
   /*                                     y = other.y; */
   /* 				      z = other.z; ) */
   
-  inline Vec3& operator =( const Vec3& other ) { x = other.x;
+  __forceinline Vec3& operator =( const Vec3& other ) { x = other.x;
 						 y = other.y;
 						 z = other.z;
 						 return *this; }
 
-  /* inline Vec3& operator =( const Vec3fa& other ) { x = other.x; */
+  /* __forceinline Vec3& operator =( const Vec3fa& other ) { x = other.x; */
   /* 						 y = other.y; */
   /* 						 z = other.z; */
   /* 						 return *this; } */
 
 
-  inline const T& operator []( const size_t axis) const { assert(axis < 3); return (&x)[axis]; }
-  inline       T& operator []( const size_t axis)       { assert(axis < 3); return (&x)[axis]; }
+  __forceinline const T& operator []( const size_t axis) const { assert(axis < 3); return (&x)[axis]; }
+  __forceinline       T& operator []( const size_t axis)       { assert(axis < 3); return (&x)[axis]; }
 
-  inline void normalize() { T len = length();
+  __forceinline void normalize() { T len = length();
                             len = len < min_rcp_input ? min_rcp_input : len;
                             x /= len; y /= len; z /= len; }
 
 
 
-  inline Vec3( const T x, const T y, const T z) : x(x), y(y), z(z) {}
+  __forceinline Vec3( const T x, const T y, const T z) : x(x), y(y), z(z) {}
   
-  inline Vec3( const T v[3] ) : x(v[0]), y(v[1]), z(v[2]) {}
+  __forceinline Vec3( const T v[3] ) : x(v[0]), y(v[1]), z(v[2]) {}
 
-  inline Vec3( const T v ) : x(v), y(v), z(v) {}
+  __forceinline Vec3( const T v ) : x(v), y(v), z(v) {}
 
-  inline T length() const { return sqrtf(x*x + y*y + z*z); }
+  __forceinline T length() const { return sqrtf(x*x + y*y + z*z); }
   
   template<typename t>
   friend std::ostream& operator <<(std::ostream &os, Vec3<t> const&v);
@@ -60,71 +60,71 @@ template<typename T> struct Vec3 {
 
 
 template<typename T>
-inline Vec3<T> operator +( const Vec3<T> &a ) { return Vec3<T>(+a.x, +a.y, +a.z); }
+__forceinline Vec3<T> operator +( const Vec3<T> &a ) { return Vec3<T>(+a.x, +a.y, +a.z); }
 template<typename T>
-inline Vec3<T> operator -( const Vec3<T> &a ) { return Vec3<T>(-a.x, -a.y, -a.z); }
+__forceinline Vec3<T> operator -( const Vec3<T> &a ) { return Vec3<T>(-a.x, -a.y, -a.z); }
 
 template<typename T>
-inline Vec3<T> operator +( const Vec3<T> &a, const Vec3<T> &b ) { return Vec3<T>(a.x+b.x, a.y+b.y, a.z+b.z); }
+__forceinline Vec3<T> operator +( const Vec3<T> &a, const Vec3<T> &b ) { return Vec3<T>(a.x+b.x, a.y+b.y, a.z+b.z); }
 template<typename T>
-inline Vec3<T> operator -( const Vec3<T> &a, const Vec3<T> &b ) { return Vec3<T>(a.x-b.x, a.y-b.y, a.z-b.z); }
+__forceinline Vec3<T> operator -( const Vec3<T> &a, const Vec3<T> &b ) { return Vec3<T>(a.x-b.x, a.y-b.y, a.z-b.z); }
 
 template<typename T>
-inline Vec3<T> operator *( const Vec3<T>& a, const Vec3<T>& b ) { return Vec3<T>(a.x * b.x, a.y * b.y, a.z * b.z); }
+__forceinline Vec3<T> operator *( const Vec3<T>& a, const Vec3<T>& b ) { return Vec3<T>(a.x * b.x, a.y * b.y, a.z * b.z); }
 template<typename T>
-inline Vec3<T> operator *( const       T& a, const Vec3<T>& b ) { return Vec3<T>(a   * b.x, a   * b.y, a   * b.z); }
+__forceinline Vec3<T> operator *( const       T& a, const Vec3<T>& b ) { return Vec3<T>(a   * b.x, a   * b.y, a   * b.z); }
 template<typename T>
-inline Vec3<T> operator *( const Vec3<T>& a, const       T& b ) { return Vec3<T>(a.x * b  , a.y * b  , a.z * b  ); }
+__forceinline Vec3<T> operator *( const Vec3<T>& a, const       T& b ) { return Vec3<T>(a.x * b  , a.y * b  , a.z * b  ); }
 template<typename T>
-inline Vec3<T> operator /( const Vec3<T>& a, const       T& b ) { return Vec3<T>(a.x / b  , a.y / b  , a.z / b  ); }
+__forceinline Vec3<T> operator /( const Vec3<T>& a, const       T& b ) { return Vec3<T>(a.x / b  , a.y / b  , a.z / b  ); }
 template<typename T>
-inline Vec3<T> operator /( const       T& a, const Vec3<T>& b ) { return Vec3<T>(a   / b.x, a   / b.y, a   / b.z); }
+__forceinline Vec3<T> operator /( const       T& a, const Vec3<T>& b ) { return Vec3<T>(a   / b.x, a   / b.y, a   / b.z); }
 template<typename T>
-inline Vec3<T> operator /( const Vec3<T>& a, const Vec3<T>& b ) { return Vec3<T>(a.x / b.x, a.y / b.y, a.z / b.z); }
+__forceinline Vec3<T> operator /( const Vec3<T>& a, const Vec3<T>& b ) { return Vec3<T>(a.x / b.x, a.y / b.y, a.z / b.z); }
 
 
 template<typename T>
-inline Vec3<bool> ge_mask( const Vec3<T> &a, const Vec3<T> &b ) { return Vec3<bool>(a.x >= b.x,a.y >= b.y, a.z >= b.z); }
+__forceinline Vec3<bool> ge_mask( const Vec3<T> &a, const Vec3<T> &b ) { return Vec3<bool>(a.x >= b.x,a.y >= b.y, a.z >= b.z); }
 
 template<typename T>
-inline Vec3<bool> le_mask( const Vec3<T> &a, const Vec3<T> &b) { return Vec3<bool>(a.x <= b.x,a.y <= b.y, a.z <= b.z); }
+__forceinline Vec3<bool> le_mask( const Vec3<T> &a, const Vec3<T> &b) { return Vec3<bool>(a.x <= b.x,a.y <= b.y, a.z <= b.z); }
 
 template<typename T>
-inline Vec3<T> min(const Vec3<T>& a, const Vec3<T>& b) { return Vec3<T>(std::min(a.x,b.x), std::min(a.y,b.y), std::min(a.z,b.z)); }
+__forceinline Vec3<T> min(const Vec3<T>& a, const Vec3<T>& b) { return Vec3<T>(std::min(a.x,b.x), std::min(a.y,b.y), std::min(a.z,b.z)); }
 
 template<typename T>
-inline Vec3<T> max(const Vec3<T>& a, const Vec3<T>& b) { return Vec3<T>(std::max(a.x,b.x), std::max(a.y,b.y), std::max(a.z,b.z)); }
+__forceinline Vec3<T> max(const Vec3<T>& a, const Vec3<T>& b) { return Vec3<T>(std::max(a.x,b.x), std::max(a.y,b.y), std::max(a.z,b.z)); }
 
 template<typename T>
-inline T reduce_add( const Vec3<T> &v ) { return v.x + v.y + v.z; }
+__forceinline T reduce_add( const Vec3<T> &v ) { return v.x + v.y + v.z; }
 
 template<typename T>
-inline T reduce_mul( const Vec3<T>& v ) { return v.x * v.y * v.z; }
+__forceinline T reduce_mul( const Vec3<T>& v ) { return v.x * v.y * v.z; }
 
 template<typename T>
-inline T reduce_min( const Vec3<T>& v ) { return std::min(std::min(v.x, v.y), v.z); }
+__forceinline T reduce_min( const Vec3<T>& v ) { return std::min(std::min(v.x, v.y), v.z); }
 
 template<typename T>
-inline T reduce_max( const Vec3<T>& v ) { return std::max(std::max(v.x, v.y), v.z); }
+__forceinline T reduce_max( const Vec3<T>& v ) { return std::max(std::max(v.x, v.y), v.z); }
 
-inline bool all(bool b[3]) { return b[0] && b[1] && b[2]; }
+__forceinline bool all(bool b[3]) { return b[0] && b[1] && b[2]; }
 
 typedef Vec3<float> Vec3f;
 typedef Vec3<int> Vec3i;
 
 template<typename T>
-inline std::ostream& operator <<(std::ostream &os, Vec3<T> const& v) {
+__forceinline std::ostream& operator <<(std::ostream &os, Vec3<T> const& v) {
   return os << '[' << v[0] << ' ' << v[1] << ' ' << v[2] << ']';
 }
 
 template<typename T>
-inline bool all(Vec3<T> v) { return v[0] && (v[0] == v[1]) && (v[0] == v[2]); }
+__forceinline bool all(Vec3<T> v) { return v[0] && (v[0] == v[1]) && (v[0] == v[2]); }
 
 template<typename T>
-inline T halfArea(Vec3<T> v) { return v.x*(v.y+v.z)+(v.y*v.z); }
+__forceinline T halfArea(Vec3<T> v) { return v.x*(v.y+v.z)+(v.y*v.z); }
 
 template<typename T>
-inline Vec3<T> zero_fix( const Vec3<T>& a )
+__forceinline Vec3<T> zero_fix( const Vec3<T>& a )
   {
     return Vec3<T>(fabs(a.x) < min_rcp_input ? T(min_rcp_input) : a.x,
                    fabs(a.y) < min_rcp_input ?  T(min_rcp_input) : a.y,
@@ -132,17 +132,17 @@ inline Vec3<T> zero_fix( const Vec3<T>& a )
   }
 
 /* template<typename T> */
-/* inline const Vec3<T> rcp(const Vec3<T>& v ) { return Vec3<T>(v.x == 0 ? inf : 1.0f/v.x, */
+/* __forceinline const Vec3<T> rcp(const Vec3<T>& v ) { return Vec3<T>(v.x == 0 ? inf : 1.0f/v.x, */
 /* 						v.y == 0 ? inf : 1.0f/v.y, */
 /* 						v.z == 0 ? inf : 1.0f/v.z); } */
 
 template<typename T>
-inline const Vec3<T> rcp(const Vec3<T>& v ) { return Vec3<T>(1.0f/v.x,
+__forceinline const Vec3<T> rcp(const Vec3<T>& v ) { return Vec3<T>(1.0f/v.x,
 							     1.0f/v.y,
 							     1.0f/v.z); }
 
 template<typename T>
-inline const Vec3<T> rcp_safe(const Vec3<T>& a) { return rcp(zero_fix(a)); }
+__forceinline const Vec3<T> rcp_safe(const Vec3<T>& a) { return rcp(zero_fix(a)); }
 
 
 
--- src/Vec3da.h
+++ src/Vec3da.h
@@ -5,6 +5,7 @@
 #include <math.h>
 #include "constants.h"
 #include "Vec3ba.h"
+#include "sys.h"
 
 struct Vec3da {
   typedef double Scalar;
@@ -12,89 +13,89 @@ struct Vec3da {
   double x,y,z;
   int a;
 
-  inline Vec3da () {}
+  __forceinline Vec3da () {}
 
-  inline Vec3da            ( const Vec3da& other ) { x = other.x; y = other.y; z = other.z; a = other.a; }
-  inline Vec3da& operator =( const Vec3da& other ) { x = other.x; y = other.y; z = other.z; a = other.a; return *this;}
+  __forceinline Vec3da            ( const Vec3da& other ) { x = other.x; y = other.y; z = other.z; a = other.a; }
+  __forceinline Vec3da& operator =( const Vec3da& other ) { x = other.x; y = other.y; z = other.z; a = other.a; return *this;}
 
-  inline Vec3da( const double pa ) { x = pa; y = pa; z = pa; a = pa;}
-  inline Vec3da( const double pa[3]) { x = pa[0]; y = pa[1]; z = pa[2]; }
-  inline Vec3da( const double px, const double py, const double pz) { x = px; y = py; z = pz; a = pz;}
+  __forceinline Vec3da( const double pa ) { x = pa; y = pa; z = pa; a = pa;}
+  __forceinline Vec3da( const double pa[3]) { x = pa[0]; y = pa[1]; z = pa[2]; }
+  __forceinline Vec3da( const double px, const double py, const double pz) { x = px; y = py; z = pz; a = pz;}
 
-  inline Vec3da( const double px, const double py, const double pz, const int pa) { x = px; y = py; z = pz; a = pa; }
+  __forceinline Vec3da( const double px, const double py, const double pz, const int pa) { x = px; y = py; z = pz; a = pa; }
     
-  inline Vec3da( ZeroTy ) { x = 0.0f; y = 0.0f; z = 0.0f; a = 0;}
-  inline Vec3da( PosInfTy ) { x = inf; y = inf; z = inf; a = inf; };
-  inline Vec3da( NegInfTy ) { x = neg_inf; y = neg_inf; z = neg_inf; a = neg_inf; };
+  __forceinline Vec3da( ZeroTy ) { x = 0.0f; y = 0.0f; z = 0.0f; a = 0;}
+  __forceinline Vec3da( PosInfTy ) { x = inf; y = inf; z = inf; a = inf; };
+  __forceinline Vec3da( NegInfTy ) { x = neg_inf; y = neg_inf; z = neg_inf; a = neg_inf; };
 
-  inline const double& operator[](const size_t index) const { assert(index < 3); return (&x)[index]; }
-  inline       double& operator[](const size_t index)       { assert(index < 3); return (&x)[index]; }
+  __forceinline const double& operator[](const size_t index) const { assert(index < 3); return (&x)[index]; }
+  __forceinline       double& operator[](const size_t index)       { assert(index < 3); return (&x)[index]; }
 
-  inline double length () const { return sqrt(x*x + y*y + z*z); }
+  __forceinline double length () const { return sqrt(x*x + y*y + z*z); }
 
-  inline Vec3da normalize() { double len = length();
+  __forceinline Vec3da normalize() { double len = length();
     len = len < min_rcp_input ? min_rcp_input : len;
     x /= len; y /= len; z/= len; }
   
 };
 
 
-inline Vec3da operator +( const Vec3da& b, const Vec3da& c ) { return Vec3da(b.x+c.x, b.y+c.y, b.z+c.z, b.a+c.a); }
-inline Vec3da operator -( const Vec3da& b, const Vec3da& c ) { return Vec3da(b.x-c.x, b.y-c.y, b.z-c.z, b.a-c.a); }
-inline Vec3da operator *( const Vec3da& b, const Vec3da& c ) { return Vec3da(b.x*c.x, b.y*c.y, b.z*c.z, b.a*c.a); }
-inline Vec3da operator *( const double& pa, const Vec3da& c ) { return Vec3da(pa) * c; }
-inline Vec3da operator *( const Vec3da& c, const double& pa ) { return Vec3da(pa) * c; }
-inline Vec3da operator /( const Vec3da& b, const Vec3da& c ) { return Vec3da(b.x/c.x, b.y/c.y, b.z/c.z, b.a/c.a); }
-inline Vec3da operator /( const double& pa, const Vec3da& c ) { return Vec3da(pa) / c; }
-inline Vec3da operator /( const Vec3da& c, const double& pa ) { return c / Vec3da(pa); }
+__forceinline Vec3da operator +( const Vec3da& b, const Vec3da& c ) { return Vec3da(b.x+c.x, b.y+c.y, b.z+c.z, b.a+c.a); }
+__forceinline Vec3da operator -( const Vec3da& b, const Vec3da& c ) { return Vec3da(b.x-c.x, b.y-c.y, b.z-c.z, b.a-c.a); }
+__forceinline Vec3da operator *( const Vec3da& b, const Vec3da& c ) { return Vec3da(b.x*c.x, b.y*c.y, b.z*c.z, b.a*c.a); }
+__forceinline Vec3da operator *( const double& pa, const Vec3da& c ) { return Vec3da(pa) * c; }
+__forceinline Vec3da operator *( const Vec3da& c, const double& pa ) { return Vec3da(pa) * c; }
+__forceinline Vec3da operator /( const Vec3da& b, const Vec3da& c ) { return Vec3da(b.x/c.x, b.y/c.y, b.z/c.z, b.a/c.a); }
+__forceinline Vec3da operator /( const double& pa, const Vec3da& c ) { return Vec3da(pa) / c; }
+__forceinline Vec3da operator /( const Vec3da& c, const double& pa ) { return c / Vec3da(pa); }
 
 
-inline bool operator ==( const Vec3da& b, const Vec3da& c) { return b.x == c.x &&
+__forceinline bool operator ==( const Vec3da& b, const Vec3da& c) { return b.x == c.x &&
 							            b.y == c.y &&
 							            b.z == c.z;
                                                            }
 
-inline const Vec3da min( const Vec3da& b, const Vec3da& c ) { return Vec3da(std::min(b.x,c.x),std::min(b.y,c.y),
+__forceinline const Vec3da min( const Vec3da& b, const Vec3da& c ) { return Vec3da(std::min(b.x,c.x),std::min(b.y,c.y),
 									   std::min(b.z,c.z),std::min(b.a,c.a)); }
-inline const Vec3da max( const Vec3da& b, const Vec3da& c ) { return Vec3da(std::max(b.x,c.x),std::max(b.y,c.y),
+__forceinline const Vec3da max( const Vec3da& b, const Vec3da& c ) { return Vec3da(std::max(b.x,c.x),std::max(b.y,c.y),
 									   std::max(b.z,c.z),std::max(b.a,c.a)); }
 
-inline const Vec3ba ge_mask( const Vec3da& b, const Vec3da& c ) { return Vec3ba(b.x >= c.x,b.y >= c.y,b.z >= c.z,b.a >= c.a); }
-inline const Vec3ba le_mask( const Vec3da& b, const Vec3da& c ) { return Vec3ba(b.x <= c.x,b.y <= c.y,b.z <= c.z,b.a <= c.a); }
+__forceinline const Vec3ba ge_mask( const Vec3da& b, const Vec3da& c ) { return Vec3ba(b.x >= c.x,b.y >= c.y,b.z >= c.z,b.a >= c.a); }
+__forceinline const Vec3ba le_mask( const Vec3da& b, const Vec3da& c ) { return Vec3ba(b.x <= c.x,b.y <= c.y,b.z <= c.z,b.a <= c.a); }
 
-inline double reduce_add( const Vec3da &v ) { return v.x + v.y + v.z; }
+__forceinline double reduce_add( const Vec3da &v ) { return v.x + v.y + v.z; }
 
 
-inline double reduce_mul( const Vec3da& v ) { return v.x * v.y * v.z; }
+__forceinline double reduce_mul( const Vec3da& v ) { return v.x * v.y * v.z; }
 
-inline double reduce_min( const Vec3da& v ) { return std::min(std::min(v.x, v.y), v.z); }
+__forceinline double reduce_min( const Vec3da& v ) { return std::min(std::min(v.x, v.y), v.z); }
 
-inline double reduce_max( const Vec3da& v ) { return std::max(std::max(v.x, v.y), v.z); }
+__forceinline double reduce_max( const Vec3da& v ) { return std::max(std::max(v.x, v.y), v.z); }
 
-inline double halfArea(Vec3da v) { return v.x*(v.y+v.z)+(v.y*v.z); }
+__forceinline double halfArea(Vec3da v) { return v.x*(v.y+v.z)+(v.y*v.z); }
 
-inline Vec3da zero_fix( const Vec3da& a )
+__forceinline Vec3da zero_fix( const Vec3da& a )
   {
     return Vec3da(fabs(a.x) < min_rcp_input ? double(min_rcp_input) : a.x,
                    fabs(a.y) < min_rcp_input ?  double(min_rcp_input) : a.y,
                    fabs(a.z) < min_rcp_input ? double(min_rcp_input) : a.z);
   }
 
-inline const Vec3da rcp(const Vec3da& v ) { return Vec3da(1.0f/v.x,
+__forceinline const Vec3da rcp(const Vec3da& v ) { return Vec3da(1.0f/v.x,
 							     1.0f/v.y,
 							     1.0f/v.z); }
 
-inline const Vec3da rcp_safe(const Vec3da& a) { return rcp(zero_fix(a)); }
+__forceinline const Vec3da rcp_safe(const Vec3da& a) { return rcp(zero_fix(a)); }
 
-inline Vec3da operator +( const Vec3da &a ) { return Vec3da(+a.x, +a.y, +a.z); }
+__forceinline Vec3da operator +( const Vec3da &a ) { return Vec3da(+a.x, +a.y, +a.z); }
 
-inline Vec3da operator -( const Vec3da &a ) { return Vec3da(-a.x, -a.y, -a.z); }
+__forceinline Vec3da operator -( const Vec3da &a ) { return Vec3da(-a.x, -a.y, -a.z); }
 
-inline double dot( const Vec3da& a, const Vec3da& b ) { return reduce_add(a*b); }
+__forceinline double dot( const Vec3da& a, const Vec3da& b ) { return reduce_add(a*b); }
 
 
 
-inline std::ostream& operator <<(std::ostream &os, Vec3da  const& v) {
+__forceinline std::ostream& operator <<(std::ostream &os, Vec3da  const& v) {
   return os << '[' << v[0] << ' ' << v[1] << ' ' << v[2] << ' ' << v.a << ']';
 }
 
