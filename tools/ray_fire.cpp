b0a0a68b0f622c36220542e0c104041436288db0
shriwise <pshriwise@gmail.com>
2018-03-15 18:07:34 -0500

    Updates to the ray fire program.

--- tools/ray_fire.cpp
+++ tools/ray_fire.cpp
@@ -16,7 +16,7 @@
 moab::ErrorCode set_volume(moab::Interface* MBI, int vol_id, moab::EntityHandle &volume) {
 
   moab::ErrorCode rval;
-  
+
   // obtain the specified volume
   moab::Tag gid_tag;
   rval = MBI->tag_get_handle(GLOBAL_ID_TAG_NAME, gid_tag);
@@ -55,16 +55,16 @@ int main(int argc, char** argv) {
   ProgOptions po("A tool for firing rays on a DagMC geometry using a SIMD BVH.");
 
   //declare options
-  
+
   std::string filename;
   po.addRequiredArg<std::string>("MOAB Model", "Filename of the MOAB model.", &filename);
-  
+
   bool build_stats = false;
   po.addOpt<void>("build-stats,s", "Print the BVH structural statistics", &build_stats);
 
   bool trav_stats = false;
   po.addOpt<void>("trav-stats,S",  "Track and print BVH traversal statistics", &trav_stats);
-  
+
   int vol_gid = 1;
   po.addOpt<int>("i",              "Specify the volume upon which to test ray intersections (default 1)", &vol_gid);
 
@@ -91,14 +91,14 @@ int main(int argc, char** argv) {
   po.addOpt<double>("cu",  "Specify the x-direction of single ray generation");
   po.addOpt<double>("cv",  "Specify the y-direction of single ray generation");
   po.addOpt<double>("cw",  "Specify the z-direction of single ray generation");
-  
+
   std::string python_dict;
   po.addOpt<std::string>("p", "if present, save parameters and results to a python dictionary file", &python_dict);
 
   po.addOptionHelpHeading("NOTE: any user-specified directions will be normalized before use");
 
   po.parseCommandLine(argc, argv);
-  
+
   // create the MOAB instance and load the file
   moab::Interface* MBI = new moab::Core();
   rval = MBI->load_file(filename.c_str());
@@ -108,7 +108,7 @@ int main(int argc, char** argv) {
   MBVHManager* BVHManager = new MBVHManager(MBI);
   rval = BVHManager->build_all();
   MB_CHK_SET_ERR(rval, "Failed to build trees");
-  
+
   moab::EntityHandle volume;
   rval = set_volume(MBI, vol_gid, volume);
   MB_CHK_SET_ERR(rval, "Failed to get and set the volume");
@@ -121,7 +121,7 @@ int main(int argc, char** argv) {
   int rays_fired = 0;
   std::clock_t start;
   double duration = 0.0;
-  
+
   // fire specified ray, if any
   if( po.getOpt("cx", ray_center)   && po.getOpt("cy", ray_center+1) && po.getOpt("cz", ray_center+2) &&
       po.getOpt("cu", ray_dir)      && po.getOpt("cv", ray_dir+1)    && po.getOpt("cw", ray_dir+2) ) {
@@ -143,38 +143,58 @@ int main(int argc, char** argv) {
 
     // report timing, memory, etc.
     report(rays_missed, rays_fired, duration);
-    
+
   }
   // if the user provided a ray file,
   // then fire the rays from there and exit
   else if ( po.getOpt("rf", &ray_file) ) {
 
-    std::ifstream file(ray_file);
-    
-    std::string line;
+    // delimiter we're searching for in the CSV file
     std::string comma = ",";
+
+    // open file
+    std::ifstream file(ray_file);
+    // object for storing line and ray values
+    std::string line;
     double r[6];
+
+    // loop over all lines in the file
     while( file.good() ){
+      // extract the next line from the file
       getline(file, line);
       int index = 0;
+      // extract all ray values from the line
       while( line.size() ) {
+	// make sure our index hasn't gotten too high
+	if( index >= 6 ) {
+	  std::cout << "Too many values found in a row of the CSV file." << std::endl;
+	  return 1;
+	}
+	// find the next comma
 	size_t it = line.find(comma);
-	if (it == std::string::npos) {
+	// if no comma is found, assume we're at the end of the line,
+	// treating the remainder of the line as a value
+	if (it != std::string::npos) {
+	  // extract the value from the line and store
+	  std::string val = line.substr(0, it);
+	  r[index] = stod(val);
+	  // remove value and delimiter from the line
+	  line.erase(0, it + comma.length());
+	  // increment index
+	  index++;
+	}
+	else {
+	  // grab the last value in the line
 	  r[index] = stod(line);
 	  break;
 	}
-      std::string val = line.substr(0, it);
-      line.erase(0, it + comma.length());
-      r[index] = stod(val);
-      index++;
       }
 
+      // construct a ray from the parsed values
       MBRay ray;
       ray.org = Vec3da(r[0], r[1], r[2]);
       ray.dir = Vec3da(r[3], r[4], r[5]);
       ray.instID = volume;
-      ray.tfar = inf;
-      ray.tnear = 0.0;
 
       rays_fired++;
       start = std::clock();
@@ -193,15 +213,15 @@ int main(int argc, char** argv) {
 
     MBRay ray;
     moab::CartVect org, dir;
-    
+
     org = moab::CartVect(rand_ray_center);
     for(int i = 0; i < num_rand_rays; i++){
       RNDVEC(dir);
-      
+
       if( rand_ray_radius > 0.0) {
 	org = dir * rand_ray_radius + moab::CartVect(rand_ray_center);
       }
-      
+
       // create the ray
       ray = MBRay(org.array(), dir.array());
       ray.instID = volume;
@@ -217,24 +237,24 @@ int main(int argc, char** argv) {
   }
 
   report(rays_missed, rays_fired, duration);
-  
+
   return rval;
 }
 
 void report(int rays_missed, int num_rays_fired, double duration) {
   /// REPORTING ///
   std::cout << std::endl;
-  
+
   // repotr missed rays
   if(rays_missed) {
     std::cout << "Warning: " << rays_missed << " random rays did not hit the target volume" << std::endl;
   }
-  
+
   // report on random rays if any were fired
   if(num_rays_fired > 0 ) {
     std::cout << "Total time per ray fire: " << duration / (double)CLOCKS_PER_SEC / num_rays_fired << " sec" << std::endl;
   }
-  
+
   // display estimate of program memory usage
-  report_memory_usage();    
+  report_memory_usage();
 }
