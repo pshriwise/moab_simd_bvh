d2e574ec57c4b7b8c997212b0f5a401e67160ef1
shriwise <pshriwise@gmail.com>
2018-03-21 00:26:29 -0500

    Updates to test values to account for lack of sign changes and ordering by largest eigenvalue. Confirmed values using wolfram alpha.

--- tests/test_mat3.cpp
+++ tests/test_mat3.cpp
@@ -31,16 +31,18 @@ int main(int argc, char** argv) {
   float lambda_check[3];
   lambda_check[0] = 3.41421; lambda_check[1] = 2.0; lambda_check[2] = 0.585786; 
   Vec3fa vec0_check(0.5, -0.707107, 0.5);
-  Vec3fa vec1_check(0.707107, 3.37748e-17, -0.707107);
+  Vec3fa vec1_check(-0.707107, 3.37748e-17, 0.707107);
   Vec3fa vec2_check(0.5, 0.707107, 0.5);  
   //now verfy that the returns Eigenvalues and Eigenvectors are correct (within some tolerance)
   float tol = 1e-04;
 
-  CHECK_REAL_EQUAL(lambda[0], lambda_check[0], tol);
-  CHECK_REAL_EQUAL(lambda[1], lambda_check[1], tol);
-  CHECK_REAL_EQUAL(lambda[2], lambda_check[2], tol);
+  CHECK_REAL_EQUAL(lambda_check[0], lambda[0], tol);
+  CHECK_REAL_EQUAL(lambda_check[1], lambda[1], tol);
+  CHECK_REAL_EQUAL(lambda_check[2], lambda[2], tol);
 
-  CHECK_VECREAL_EQUAL_TOL(vectors[0], vec0_check, tol);
+  CHECK_VECREAL_EQUAL_TOL(vec0_check, vectors[0], tol);
+  CHECK_VECREAL_EQUAL_TOL(vec1_check, vectors[1], tol);
+  CHECK_VECREAL_EQUAL_TOL(vec2_check, vectors[2], tol);
   
   return 0;
 }
