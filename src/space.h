b589eb87f5b6cf08335415202a18304a6d76f2bc
shriwise <pshriwise@gmail.com>
2018-04-01 13:41:56 -0500

    Updates to AffineSpace to allow for node building

--- src/space.h
+++ src/space.h
@@ -59,6 +59,14 @@ struct LinSpaceT {
 
   __forceinline LinSpaceT operator /=(const Scalar& v) { vx = vx / v; vy = vy / v; vz = vz / v; return *this; }
 
+  // create matrix for scaling
+  static __forceinline LinSpaceT scale(const T &v) {
+    return LinSpaceT(v.x, 0.0f, 0.0f,
+		     0.0f, v.y, 0.0f,
+		     0.0f, 0.0f, v.z);
+  }
+
+  
   /* column vectors of the matrix */
   T vx, vy, vz;
 };
@@ -83,6 +91,10 @@ __forceinline LinSpaceT<T> operator -( const LinSpaceT<T>& a, const LinSpaceT<T>
 template<typename T>
 __forceinline LinSpaceT<T> operator *( const typename T::Scalar& a, const LinSpaceT<T>& b) { return LinSpaceT<T>(a*b.vx, a*b.vy, a*b.vz); }
 
+
+template<typename T>
+__forceinline LinSpaceT<T> operator *( const float& a, const LinSpaceT<T>& b) { return LinSpaceT<T>(a*b.vx, a*b.vy, a*b.vz); }
+
 template<typename T>
 __forceinline T operator *( const LinSpaceT<T>& a, const T& b) { return madd(T(b.x),a.vx,madd(T(b.y),a.vy,T(b.z)*a.vz)); }
 
@@ -98,14 +110,6 @@ template<typename T>
 __forceinline bool operator !=(const LinSpaceT<T>& a, const LinSpaceT<T>& b) { return a.vx != b.vx || a.vy != b.vy || a.vz != b.vz; }
 
 /// SPATIAL OPERATORS ///
-
-// create matrix for scaling
-template<typename T>
-static __forceinline LinSpaceT<T> scale(const T &v) {
-  return LinSpaceT<T>(v.x, 0.0f, 0.0f,
-		  0.0f, v.y, 0.0f,
-		  0.0f, 0.0f, v.z);
-}
 
 
 // rotation around an arbitrary axis in rads
@@ -141,63 +145,91 @@ typedef LinSpaceT<Vec3fa> LinSpace;
 typedef LinSpaceT<Vec3vfa> LinSpaceV;
 
 
-#define V typename LinSpaceV::Vector
+#define V typename L::Vector
 
-struct AffineSpace {
+template< typename L >
+struct AffineSpaceT {
   
-  __forceinline AffineSpace () {}
+  __forceinline AffineSpaceT () {}
 
-  __forceinline AffineSpace ( const AffineSpace& other ) { l = other.l; p = other.p; }
+  __forceinline AffineSpaceT ( const AffineSpaceT& other ) { l = other.l; p = other.p; }
   
-  __forceinline AffineSpace& operator=( const AffineSpace& other ) { l = other.l; p = other.p; return *this; }
+  __forceinline AffineSpaceT& operator=( const AffineSpaceT& other ) { l = other.l; p = other.p; return *this; }
     
-  __forceinline AffineSpace ( const Vec3vfa& vx, const Vec3vfa& vy, const Vec3vfa& vz, const Vec3vfa& p ) : l(vx,vy,vz) , p(p) {}
+  __forceinline AffineSpaceT ( const Vec3vfa& vx, const Vec3vfa& vy, const Vec3vfa& vz, const Vec3vfa& p ) : l(vx,vy,vz) , p(p) {}
 
-  __forceinline AffineSpace ( const LinSpaceV& l, const Vec3vfa& p ) : l(l), p(p) {}
+  __forceinline AffineSpaceT ( const L& l, const V& p ) : l(l), p(p) {}
 
-  __forceinline AffineSpace ( ZeroTy ) : l(zero), p(zero) {}
-  
-  /* __forceinline AffineSpace ( OneTy )  : l(one),  p(zero)  {} */
+  __forceinline AffineSpaceT ( const L& l ) : l(l), p(zero) {}
 
-  /* __forceinline AffineSpace scale(const Vec3vfa& s) { return LinSpaceV::scale(s); } */
+  __forceinline AffineSpaceT ( ZeroTy ) : l(zero), p(zero) {}
   
-  /* __forceinline AffineSpace rotat(const float&  r) { return LinSpaceV::rotate(r); } */
+  /* __forceinline AffineSpaceT ( OneTy )  : l(one),  p(zero)  {} */
+
+  static __forceinline const AffineSpaceT scale(const V& v) { return L::scale(v); }
+
   
-  LinSpaceV l;
-  LinSpaceV::Vector p;
+  /* __forceinline AffineSpaceT scale(const Vec3vfa& s) { return L::scale(s); } */
+  
+  /* __forceinline AffineSpaceT rotat(const float&  r) { return L::rotate(r); } */
+  
+  L l;
+  V p;
   
 };
 
-__forceinline bool operator ==( const AffineSpace& a, const AffineSpace& b ) { return a.l == b.l && a.p == b.p; }
-__forceinline bool operator !=( const AffineSpace& a, const AffineSpace& b ) { return a.l != b.l || a.p != b.p; }
+template<typename L>
+__forceinline bool operator ==( const AffineSpaceT<L>& a, const AffineSpaceT<L>& b ) { return a.l == b.l && a.p == b.p; }
+template<typename L>
+__forceinline bool operator !=( const AffineSpaceT<L>& a, const AffineSpaceT<L>& b ) { return a.l != b.l || a.p != b.p; }
 
-__forceinline AffineSpace operator -( const AffineSpace& a ) { return AffineSpace( -a.l, -a.p ); }
-__forceinline AffineSpace operator +( const AffineSpace& a ) { return AffineSpace( +a.l, +a.p ); }
-__forceinline AffineSpace        rcp( const AffineSpace& a ) { LinSpaceV il = rcp(a.l); return AffineSpace(il, -(il*a.p)); }
+template<typename L>
+__forceinline AffineSpaceT<L> operator -( const AffineSpaceT<L>& a ) { return AffineSpaceT<L>( -a.l, -a.p ); }
 
-__forceinline AffineSpace operator +( const AffineSpace& a, const AffineSpace& b ) { return AffineSpace(a.l + b.l, a.p + b.p); }
-__forceinline AffineSpace operator -( const AffineSpace& a, const AffineSpace& b ) { return AffineSpace(a.l - b.l, a.p - b.p); }
+template<typename L>
+__forceinline AffineSpaceT<L> operator +( const AffineSpaceT<L>& a ) { return AffineSpaceT<L>( +a.l, +a.p ); }
 
-__forceinline AffineSpace operator *( const float&       a, const AffineSpace& b ) { return AffineSpace(a   * b.l, a   * b.p); }
+template<typename L>
+__forceinline AffineSpaceT<L>        rcp( const AffineSpaceT<L>& a ) { L il = rcp(a.l); return AffineSpaceT<L>(il, -(il*a.p)); }
 
-__forceinline AffineSpace operator *( const AffineSpace& a, const float&       b) { return AffineSpace(b   * a.l, b   * a.p); }
+template<typename L>
+__forceinline AffineSpaceT<L> operator +( const AffineSpaceT<L>& a, const AffineSpaceT<L>& b ) { return AffineSpaceT<L>(a.l + b.l, a.p + b.p); }
+template<typename L>
+__forceinline AffineSpaceT<L> operator -( const AffineSpaceT<L>& a, const AffineSpaceT<L>& b ) { return AffineSpaceT<L>(a.l - b.l, a.p - b.p); }
 
+template<typename L>
+__forceinline AffineSpaceT<L> operator *( const float&       a, const AffineSpaceT<L>& b ) { return AffineSpaceT<L>(a   * b.l, a   * b.p); }
 
-__forceinline AffineSpace operator *( const AffineSpace& a, const AffineSpace& b ) { return AffineSpace(a.l * b.l, a.p * b.p); }
+template<typename L>
+__forceinline AffineSpaceT<L> operator *( const AffineSpaceT<L>& a, const float&       b) { return AffineSpaceT<L>(b   * a.l, b   * a.p); }
 
-__forceinline AffineSpace operator /( const AffineSpace& a, const AffineSpace& b ) { return a * rcp(b); }
-__forceinline AffineSpace operator /( const AffineSpace& a, const float&       b ) { return a * rcp(b); }
+template<typename L>
+__forceinline AffineSpaceT<L> operator *( const AffineSpaceT<L>& a, const AffineSpaceT<L>& b ) { return AffineSpaceT<L>(a.l * b.l, a.p * b.p); }
 
-__forceinline AffineSpace& operator *=( AffineSpace& a, const AffineSpace& b ) { return a = a * b; }
-__forceinline AffineSpace& operator *=( AffineSpace& a, const float&       b ) { return a = a * b; }
-__forceinline AffineSpace& operator /=( AffineSpace& a, const AffineSpace& b ) { return a = a / b; }
-__forceinline AffineSpace& operator /=( AffineSpace& a, const float&       b ) { return a = a / b; }
+template<typename L>
+__forceinline AffineSpaceT<L> operator /( const AffineSpaceT<L>& a, const AffineSpaceT<L>& b ) { return a * rcp(b); }
 
-__forceinline const V xfmPoint (const AffineSpace& m, const V& p) { return madd(V(p.x),m.l.vx,madd(V(p.y),m.l.vy,madd(V(p.z),m.l.vz,m.p))); }
-__forceinline const V xfmVector(const AffineSpace& m, const V& v) { return xfmVector(m.l,v); }
-__forceinline const V xfmNormal(const AffineSpace& m, const Vec3vfa& n) { return xfmNormal(m.l,n); }
+template<typename L>
+__forceinline AffineSpaceT<L> operator /( const AffineSpaceT<L>& a, const float&       b ) { return a * rcp(b); }
 
-__forceinline const AABB xfmBounds(const LinSpace& l, const AABB& b) {
+template<typename L>
+__forceinline AffineSpaceT<L>& operator *=( AffineSpaceT<L>& a, const AffineSpaceT<L>& b ) { return a = a * b; }
+template<typename L>
+__forceinline AffineSpaceT<L>& operator *=( AffineSpaceT<L>& a, const float&       b ) { return a = a * b; }
+template<typename L>
+__forceinline AffineSpaceT<L>& operator /=( AffineSpaceT<L>& a, const AffineSpaceT<L>& b ) { return a = a / b; }
+template<typename L>
+__forceinline AffineSpaceT<L>& operator /=( AffineSpaceT<L>& a, const float&       b ) { return a = a / b; }
+
+template<typename L>
+__forceinline const V xfmPoint (const AffineSpaceT<L>& m, const V& p) { return madd(V(p.x),m.l.vx,madd(V(p.y),m.l.vy,madd(V(p.z),m.l.vz,m.p))); }
+template<typename L>
+__forceinline const V xfmVector(const AffineSpaceT<L>& m, const V& v) { return xfmVector(m.l,v); }
+template<typename L>
+__forceinline const V xfmNormal(const AffineSpaceT<L>& m, const Vec3vfa& n) { return xfmNormal(m.l,n); }
+
+template<typename L>
+__forceinline const AABB xfmBounds(const L& l, const AABB& b) {
   AABB dst = AABB(zero);
   const Vec3fa p0(b.lower.x, b.lower.y, b.lower.z); dst.update(xfmPnt(l, p0));
   const Vec3fa p1(b.lower.x, b.lower.y, b.upper.z); dst.update(xfmPnt(l, p1));
@@ -210,8 +242,13 @@ __forceinline const AABB xfmBounds(const LinSpace& l, const AABB& b) {
   return dst;
 }
 
+
 #undef V
 
-static std::ostream& operator<<(std::ostream& cout, const AffineSpace& m){
+template<typename L>
+static std::ostream& operator<<(std::ostream& cout, const AffineSpaceT<L>& m){
   return cout << "{ l = " << m.l << ", p = " << m.p << " }";
 }
+
+typedef AffineSpaceT<LinSpace> AffineSpace3fa;
+typedef AffineSpaceT<LinSpaceV> AffineSpace;
