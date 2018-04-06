73d306f484d2fdf608907137595ee80e72660617
shriwise <pshriwise@gmail.com>
2018-04-05 19:04:56 -0500

    Adding a few tests for the unaligned node.

--- tests/test_uanode.cpp
+++ tests/test_uanode.cpp
@@ -3,6 +3,89 @@
 #include "testutil.hpp"
 #include "Node.h"
 
+
+// returns an OBB constructed using the bounding points of an AABB
+OBB obb_from_aabb(const AABB& aabb) {
+
+  Vec3fa lo = aabb.lower;
+  Vec3fa hi = aabb.upper;
+  // generate a controlled OBB 
+  std::vector<float> xs, ys, zs;
+  xs.push_back(lo.x); ys.push_back(lo.y); zs.push_back(lo.z);
+  xs.push_back(hi.x); ys.push_back(lo.y); zs.push_back(lo.z);
+  xs.push_back(hi.x); ys.push_back(hi.y); zs.push_back(lo.z);
+  xs.push_back(lo.x); ys.push_back(hi.y); zs.push_back(lo.z);
+  xs.push_back(lo.x); ys.push_back(lo.y); zs.push_back(hi.z);
+  xs.push_back(hi.x); ys.push_back(lo.y); zs.push_back(hi.z);
+  xs.push_back(hi.x); ys.push_back(hi.y); zs.push_back(hi.z);
+  xs.push_back(lo.x); ys.push_back(hi.y); zs.push_back(hi.z);
+  
+  OBB box(&xs.front(), &ys.front(), &zs.front(), xs.size());
+
+  return box;
+}
+
 int main() {
+
+  // generate a controlled OBB 
+  std::vector<float> xs, ys, zs;
+  xs.push_back( 5.0); ys.push_back( 0.0); zs.push_back( 0.0);
+  xs.push_back( 0.0); ys.push_back( 5.0); zs.push_back( 0.0);
+  xs.push_back( 5.0); ys.push_back( 5.0); zs.push_back( 0.0);
+  xs.push_back( 0.0); ys.push_back( 0.0); zs.push_back( 0.0);
+  xs.push_back( 5.0); ys.push_back( 0.0); zs.push_back( 5.0);
+  xs.push_back( 0.0); ys.push_back( 5.0); zs.push_back( 5.0);
+  xs.push_back( 5.0); ys.push_back( 5.0); zs.push_back( 5.0);
+  xs.push_back( 0.0); ys.push_back( 0.0); zs.push_back( 5.0);
+
+  OBB box(&xs.front(), &ys.front(), &zs.front(), xs.size());
+
+  // setup a node with that box
+  UANode node = UANode();
+  node.setBounds(box);
+
+  // setup some intersection information
+  vfloat4 z(zero), i(inf), ni(neg_inf);
+  vfloat4 dist(100.0);
+  size_t result;
+
+  vfloat4 expected_dist;
+  size_t expected_result;
+  
+  // create a ray
+  Vec3fa org(10.0, 0.0, 0.0);
+  Vec3fa dir(-1.0, 0.0, 0.0);
+  TravRay r(org, dir);
+
+  // intersect the node
+  result = intersectBox(node, r, z, i, dist);
+
+  // check the results
+  expected_dist = 5.0;
+  expected_result = 15;
+  CHECK_EQUAL(expected_result, result);
+  CHECK_VFLOATREAL_EQUAL(expected_dist, dist);
+
+  // intersect the node
+  result = intersectBox(node, r, z, z, dist);
+
+  // check the results
+  expected_result = 0;
+  CHECK_EQUAL(expected_result, result);
+
+  // setup a new ray (reversed direction)
+  r.dir = -r.dir;
+
+  // intersect the node
+  result = intersectBox(node, r, z, i, dist);
+
+  // check the results
+  expected_result = 0;
+  CHECK_EQUAL(expected_result, result);
+
+  
+  
+  
+  
   return 0;
 }
