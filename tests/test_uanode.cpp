689f7e1b82883222032bdc4e75f0a72034a90d16
shriwise <pshriwise@gmail.com>
2018-04-05 20:41:44 -0500

    Adding more unaligned node test cases.

--- tests/test_uanode.cpp
+++ tests/test_uanode.cpp
@@ -25,8 +25,19 @@ OBB obb_from_aabb(const AABB& aabb) {
   return box;
 }
 
-int main() {
+void uniform_node_tests();
+void nonuniform_node_tests();
 
+int main() {
+ 
+  uniform_node_tests();
+  nonuniform_node_tests();
+  
+  return 0;
+}
+
+void uniform_node_tests() {
+  
   AABB aabb(Vec3fa(0.0), Vec3fa(5.0));
   OBB box = obb_from_aabb(aabb);
 
@@ -73,9 +84,116 @@ int main() {
   expected_result = 0;
   CHECK_EQUAL(expected_result, result);
 
+  return;
+}
+void nonuniform_node_tests() {
   
+  AABB aabb0(Vec3fa(0.0), Vec3fa(5.0));
+  OBB box0 = obb_from_aabb(aabb0);
+  AABB aabb1(Vec3fa(0.0), Vec3fa(4.0));
+  OBB box1 = obb_from_aabb(aabb1);
+  AABB aabb2(Vec3fa(0.0), Vec3fa(3.0));
+  OBB box2 = obb_from_aabb(aabb2);
+  AABB aabb3(Vec3fa(0.0), Vec3fa(2.0));
+  OBB box3 = obb_from_aabb(aabb3);
+
+  // setup a node with that box
+  UANode node = UANode();
+  node.setBound(0, box0);
+  node.setBound(1, box1);
+  node.setBound(2, box2);
+  node.setBound(3, box3);
+
+  // setup some intersection information
+  vfloat4 z(zero), i(inf), ni(neg_inf);
+  vfloat4 dist(100.0);
+  size_t result;
+
+  vfloat4 expected_dist;
+  size_t expected_result;
   
-  
-  
-  return 0;
+  // create a ray
+  Vec3fa org(10.0, 0.0, 0.0);
+  Vec3fa dir(-1.0, 0.0, 0.0);
+  TravRay r(org, dir);
+
+  // intersect the node
+  result = intersectBox(node, r, z, i, dist);
+
+  // check the results
+  expected_dist = vfloat4(5.0, 6.0, 7.0, 8.0);;
+  expected_result = 15;
+  CHECK_EQUAL(expected_result, result);
+  CHECK_VFLOATREAL_EQUAL(expected_dist, dist);
+
+  // intersect the node
+  result = intersectBox(node, r, z, z, dist);
+
+  // check the results
+  expected_result = 0;
+  CHECK_EQUAL(expected_result, result);
+
+  // set a non-uniform ray length
+  vfloat4 tfar(10.0, 10.0, 10.0, 0.0);
+
+  // intersect the node
+  result = intersectBox(node, r, z, tfar, dist);
+
+  // check the results
+  expected_result = 7;
+  CHECK_EQUAL(expected_result, result);
+
+  // set a non-uniform ray length
+  tfar = vfloat4(10.0, 10.0, 0.0, 0.0);
+
+  // intersect the node
+  result = intersectBox(node, r, z, tfar, dist);
+
+  // check the results
+  expected_result = 3;
+  CHECK_EQUAL(expected_result, result);
+
+  // set a non-uniform ray length
+  tfar = vfloat4(5.5, 5.5, 5.5, 5.5);
+
+  // intersect the node
+  result = intersectBox(node, r, z, tfar, dist);
+
+  // check the results
+  expected_result = 1;
+  CHECK_EQUAL(expected_result, result);
+
+  // set a non-uniform ray length
+  tfar = vfloat4(7.5, 7.5, 7.5, 7.5);
+
+  // intersect the node
+  result = intersectBox(node, r, z, tfar, dist);
+
+  // check the results
+  expected_result = 7;
+  CHECK_EQUAL(expected_result, result);
+
+  // set a non-uniform ray length
+  tfar = vfloat4(8.5, 8.5, 8.5, 8.5);
+
+  // intersect the node
+  result = intersectBox(node, r, z, tfar, dist);
+
+  // check the results
+  expected_result = 15;
+  CHECK_EQUAL(expected_result, result);
+
+  // setup a new ray (reversed direction)
+  r.dir = -r.dir;
+
+  // intersect the node
+  result = intersectBox(node, r, z, i, dist);
+
+  // check the results
+  expected_result = 0;
+  CHECK_EQUAL(expected_result, result);
+
+
+
+  return;
 }
